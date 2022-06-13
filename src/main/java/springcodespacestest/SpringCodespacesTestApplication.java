db98b6f7ba93870e1e643435a440767717352096
Morsiani Renato <m.renato@freemail.hu>
2022-06-13 15:55:53 +0200

    waiting to database added

--- src/main/java/springcodespacestest/SpringCodespacesTestApplication.java
+++ src/main/java/springcodespacestest/SpringCodespacesTestApplication.java
@@ -10,4 +10,27 @@ public class SpringCodespacesTestApplication {
         SpringApplication.run(SpringCodespacesTestApplication.class, args);
     }
 
+/*  Wait for database to initialize - example
+    @Bean
+    public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+        var dsv = new DatabaseStartupValidator();
+        dsv.setDataSource(dataSource);
+        dsv.setValidationQuery(DatabaseDriver.MARIADB.getValidationQuery());
+        return dsv;
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+        return bf -> {
+            // Let beans that need the database depend on the DatabaseStartupValidator
+            // like the JPA EntityManagerFactory or Flyway
+
+            String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+            Stream.of(jpa)
+                    .map(bf::getBeanDefinition)
+                    .forEach(it -> it.setDependsOn("databaseStartupValidator"));
+        };
+    }*/
+
+
 }
